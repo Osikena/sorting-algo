f1670f544d17a7a241c0fdf7dec096a6c6826eb8
ABDULMALIK-ADULLAHI <aabdullahi@systemspecs.com.ng>
2022-05-21 23:01:42 +0100

    Initial Commit

---
+++ sorting.js
@@ -0,0 +1,16 @@
+
+//Bubble sort
+function bubbleSort(num){
+    for(let i = 0; i < num.length; i++){
+        for(let j = 0; j < num.length - i - 1; j++){
+            if(num[j + 1] < num[j]){
+                [num[j + 1],num[j]] = [num[j],num[j + 1]]
+            }
+        }
+    };
+    return num;
+};
+
+var num = [5,3,8,4,6];
+
+console.log(bubbleSort(num));
